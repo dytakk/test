7738567862ca2c529221078f511add4fc1787dd5
dy <dy>
2021-04-17 20:58:03 +0900

    new world letsgo

---
+++ src/First/first.java
@@ -0,0 +1,13 @@
+package First;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("hello world");
+	  System.out.print("\ntomorrow is sunday");
+		
+	}
+
+}
